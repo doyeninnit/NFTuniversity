c9dc3676085abd5e439133262fed0f7399ece844
agate <agatenashons@gmail.com>
2022-07-16 09:14:02 +0300

    Solidity change
    Signed-off-by: agate <agatenashons@gmail.com>

--- universitycontract.sol
+++ universitycontract.sol
@@ -6,17 +6,38 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 
 contract UniversityContract is ERC721URIStorage {
-    address owner;
+    
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("UniversityDegree", "Degree") {
+    constructor() ERC721("NFTUniversityDegree", "NFTDegree") {
         owner = msg.sender;
     }
 
+
+    
+     //stores addresses that have been whitelisted
+    mapping(address => bool) public whitelistedAddresses;
+
+    //stores name of the student
+    mapping (address => string) public studentName;
+
+    //stores the time when an address was added
+    mapping(string => uint) public timeAddressAdded;
+
+    //stores addresses of issued degrees
     mapping(address => bool) public IssuedDegrees;
+
+    //stores a persons address to tokenURI it holds
     mapping(address => string) public PersonToDegree;
+
+    //stores address of the deployer
+     address owner;
+
+    //stores number of whitelistd addresses
+    uint8 public numAddressesWhitelisted;
+    
 
 
     modifier onlyOwner() {
@@ -24,8 +45,27 @@ contract UniversityContract is ERC721URIStorage {
         _;
     }
 
+
+    function addAddressToWhitelist(address _newAddress, string memory _name) public onlyOwner{
+
+        require(!whitelistedAddresses[_newAddress], "student already whitelisted");
+
+       //Adds the address to the mapping 
+        whitelistedAddresses[_newAddress] = true;
+
+        //Adds name of the student 
+        studentName[_newAddress] = _name;
+
+        // Adds the time when the name and address was added
+        timeAddressAdded[_name] = block.timestamp;
+        
+        numAddressesWhitelisted += 1;
+    }
+
+
     function issueDegree (address to ) onlyOwner public {
         IssuedDegrees[to] = true;
+        require(whitelistedAddresses[to], "Address not whitelisted");
     }
 
     function claimDegree(string memory tokenURI) public returns (uint256){
@@ -37,7 +77,7 @@ contract UniversityContract is ERC721URIStorage {
 
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
-
+        
         PersonToDegree[msg.sender] = tokenURI;
         IssuedDegrees[msg.sender] = false;
 
