840436dfdcf6fb85961f64e461124cea70aee239
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-07-16 17:42:22 +0300

    Add Changes to Contract

---
+++ README.md
@@ -0,0 +1,59 @@
+# NFT University Degree
+Building an NFT Project that allows universities or learning programs to issue NFT certificates to successful graduates but limit the graduates from transferring or selling the NFTs to others. 
+
+## Built With
+
+- OpenZeppelin ERC721
+- Ankr
+- Rinkeby Testnet
+
+## Live Demo
+
+- [Rinkeby Scan](https://rinkeby.etherscan.io/address/0x7a21B4Ec6DeeDD96f276Dc8b0C20AcED10d036DA)
+
+<!-- ### NFT Gallery
+
+[BuyMeACoffee](https://buymeacoffee-fe-psi.vercel.app/) -->
+
+
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+- Fork the repository
+- Git clone https://github.com/your-username/NFTuniversity
+- git checkout -b branch name
+- git remote add upstream https://github.com/Nashons/NFTuniversity
+- git pull upstream master
+- git add .
+- git commit -m "commit message"
+- git push -u origin HEAD
+
+
+### Hardhat Commands
+- npx hardhat
+- npx hardhat compile
+- npx hardhat run rinkeby scripts/deploy.js
+- npx hardhat verify --network rinkeby 0x7a21B4Ec6DeeDD96f276Dc8b0C20AcED10d036DA
+
+## Authors
+
+👤 **Authors**
+
+- Github: [@Nashons](https://github.com/Nashons
+- GitHub: [@Jebitok-dev](https://github.com/Jebitok-dev)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgment
+
+I hereby acknowledge the creators of [Africa Blockchain Center](https://theafricablockchaincenter.com/) who are the organizers of the Africa Blockchain Center Hackathon.
---
+++ contracts/NFTUniversityCertificate.sol
@@ -0,0 +1,87 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract UniversityContract is ERC721URIStorage {
+    
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("NFTUniversityDegree", "NFTDegree") {
+        owner = msg.sender;
+    }
+
+
+    
+     //stores addresses that have been whitelisted
+    mapping(address => bool) public whitelistedAddresses;
+
+    //stores name of the student
+    mapping (address => string) public studentName;
+
+    //stores the time when an address was added
+    mapping(string => uint) public timeAddressAdded;
+
+    //stores addresses of issued degrees
+    mapping(address => bool) public IssuedDegrees;
+
+    //stores a persons address to tokenURI it holds
+    mapping(address => string) public PersonToDegree;
+
+    //stores address of the deployer
+     address owner;
+
+    //stores number of whitelistd addresses
+    uint8 public numAddressesWhitelisted;
+    
+
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+
+    function addAddressToWhitelist(address _newAddress, string memory _name) public onlyOwner{
+
+        require(!whitelistedAddresses[_newAddress], "student already whitelisted");
+
+       //Adds the address to the mapping 
+        whitelistedAddresses[_newAddress] = true;
+
+        //Adds name of the student 
+        studentName[_newAddress] = _name;
+
+        // Adds the time when the name and address was added
+        timeAddressAdded[_name] = block.timestamp;
+        
+        numAddressesWhitelisted += 1;
+    }
+
+
+    function issueDegree (address to ) onlyOwner public {
+        IssuedDegrees[to] = true;
+        require(whitelistedAddresses[to], "Address not whitelisted");
+    }
+
+    function claimDegree(string memory tokenURI) public returns (uint256) {
+        require(IssuedDegrees[msg.sender], "Degree not yet issues");
+
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        
+        PersonToDegree[msg.sender] = tokenURI;
+        IssuedDegrees[msg.sender] = false;
+
+        return newItemId;
+    }
+}
